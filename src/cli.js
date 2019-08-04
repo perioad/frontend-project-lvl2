6455f6bcbcf040290218f8f93038163fddf197e6
Perio Alexander <perioad@me.com>
2019-08-04 20:16:15 +0300

    first step

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+const program = require('commander');
+
+export default () => {
+  program
+    .version('0.0.1')
+    .description('Compares two configuration files and shows a difference.')
+    .parse(process.argv);
+};
