555875325503b97ef4ff25cbe05cb7826a7c5411
Perio Alexander <perioad@me.com>
2019-08-05 17:48:26 +0300

    Flat files compare

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,20 @@
+import genDiff from '.';
+
 const program = require('commander');
 
 export default () => {
   program
     .version('0.0.1')
-    .description('Compares two configuration files and shows a difference.')
-    .parse(process.argv);
+    .description('Compares two configuration files and shows a difference.');
+
+  program
+    .option('-f, --format [type]', 'output format');
+
+  program
+    .arguments('<firstConfig> <secondConfig>')
+    .action((firstConfig, secondConfig) => {
+      genDiff(firstConfig, secondConfig);
+    });
+
+  program.parse(process.argv);
 };
